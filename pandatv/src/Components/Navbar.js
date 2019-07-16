41460a224fc2a1f2b8dc4b77171dc5f1812a96cd
Tibo <tiborkeri@icloud.com>
2019-07-16 17:20:45 +0100

    Navbar.js

---
+++ pandatv/src/Components/Navbar.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+ 
+// const Navigation = (props) => {
+//   return (
+//     <nav id="nav_bar"className="navbar navbar-expand-lg position-absolute justify-content-center">
+//       <div className="p-2 bg-white d-flex align-items-center">
+//         <div className="logoContainer">
+//           <a className="padding-right: 5rem tomatoText" href="">Panda TV</a>
+//         </div>
+//         <li><Link to={"./Home"}></Link>Home |</li>
+//         <a className="tomatoText"href="/Users/tiborkeri/Desktop/Farazad/opustique2/index.html"></a>
+//       </div>
+//       <div className="p-2 tomatoBg d-flex align-items-center">
+//         <a className="text-white pr-3"href="">Chi | Eng</a>
+//         <a className="text-white"href="">Login |</a>
+//         <a className="text-white "href="">Register</a>
+//       </div>
+//     </nav>
+//   )
+// }
+
+
+// export default Navigation;
